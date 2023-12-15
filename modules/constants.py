d22a3ab9aa838487eb9c815ef302976628b093d9
denivic <denivic@protonmail.com>
2023-12-15 23:13:26 +0100

    Consants() is now an StrEnum(), added AlgorithmType() enum as well

--- modules/constants.py
+++ modules/constants.py
@@ -1,5 +1,19 @@
 from pathlib import Path
+from enum import Enum, StrEnum
 
-class Constants():
-    TEST = fr'{Path(__file__).parents[3]}'
-    LOG_PATH = fr'{Path(__file__).parents[3]}\Diagrammer'
+class Constants(StrEnum):
+    LOG_PATH = fr'{Path(__file__).parents[1]}\logs'
+
+
+class AlgorithmType(Enum):
+    SELECTION_SORT = 1
+    BUBBLE_SORT = 2
+    INSERTION_SORT = 3
+    MERGE_SORT = 4
+    QUICK_SORT = 5
+    HEAP_SORT = 6
+    COUNTING_SORT = 7
+    RADIX_SORT = 8
+    BUCKET_SORT = 9
+    BINGO_SORT = 10
+    SHELL_SORT = 11
