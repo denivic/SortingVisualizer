58dc66e63b60d89d00be389f183cc098800aed21
denivic <denivic@protonmail.com>
2023-12-15 23:14:20 +0100

    Simplified timer() while now also checking how many arguments are given.

--- modules/utilitymanager.py
+++ modules/utilitymanager.py
@@ -4,22 +4,22 @@ from contextlib import contextmanager
 from logging import setLoggerClass, getLogger
 
 # Custom modules
-from modules.logger import CustomLogger
+from modules.logger import SVLogger
 
 class UtilityManager():
     def __init__(self):
-        setLoggerClass(CustomLogger)
+        setLoggerClass(SVLogger)
         self.logger = getLogger('__name__')
         self.logger.propagate = False  # Prevents duplicate console logs
-    
-    
+
+
     @contextmanager
     def timer(self, *args) -> None:
-        """ 
+        """
             Contextmanager that measures runtime.
             Accepts `method`, which is an optional
             name you given to the piece you're timing.\n\n
-            
+
             If `method = 'total'` then it is the total
             runtime of the snippet of code.
         """
@@ -30,7 +30,7 @@ class UtilityManager():
         finally:
             runtime = f'{perf_counter() - start:.2f}'
 
-            if 'total' in args:
-                self.logger.info(f'Total execution time: {runtime} seconds')
-            else:
+            if len(args) > 0:
                 self.logger.info(f'Execution time for {args[0]}: {runtime} seconds')
+            else:
+                self.logger.info(f'Total execution time: {runtime} seconds')
