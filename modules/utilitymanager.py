f234bfaac75f24907948a023f06b80801b645111
denivic <denivic@protonmail.com>
2023-12-28 10:00:28 +0100

    Edited the precision of the timer

--- modules/utilitymanager.py
+++ modules/utilitymanager.py
@@ -18,7 +18,7 @@ class UtilityManager():
         """
             Contextmanager that measures runtime.
             Accepts `method`, which is an optional
-            name you given to the piece you're timing.\n\n
+            name given to the piece you're timing.\n\n
 
             If `method = 'total'` then it is the total
             runtime of the snippet of code.
@@ -28,7 +28,7 @@ class UtilityManager():
         try:
             yield
         finally:
-            runtime = f'{perf_counter() - start:.2f}'
+            runtime = f'{perf_counter() - start:.6f}'
 
             if len(args) > 0:
                 self.logger.info(f'Execution time for {args[0]}: {runtime} seconds')
